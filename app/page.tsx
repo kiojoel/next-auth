1f6edef4588a233fe425889588a26fde1f52b6c4
Akinsanya Joel <akinsanyajoel82@.com>
2024-07-02 12:36:54 +0100

    added  landing page and login route

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,36 @@
+import { Poppins } from "next/font/google";
+
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import LoginButton from "../components/auth/login_button";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
 export default function Home() {
-  return <div className="text-red-500">hey Auth!</div>;
+  return (
+    <main className="flex h-full flex-col items-center justify-center bg-sky-400">
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            font.className
+          )}
+        >
+          Auth
+        </h1>
+        <p className="text-white text-lg">A simple authentication service</p>
+
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
+  );
 }
