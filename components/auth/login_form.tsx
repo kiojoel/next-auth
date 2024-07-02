a413702e227f36886b52907dba1e52b4dfa2a8dc
Akinsanya Joel <akinsanyajoel82@.com>
2024-07-02 13:59:32 +0100

    login page

--- components/auth/login_form.tsx
+++ components/auth/login_form.tsx
@@ -1,5 +1,16 @@
+import { CardWrapper } from "./card-wrapper";
+
 const LoginForm = () => {
-  return <div>Login Form</div>;
+  return (
+    <CardWrapper
+      headerLabel="Welcome back"
+      backButtonLabel="Don't have an account?"
+      backButtonHref="/auth/register"
+      showSocial
+    >
+      Login Form
+    </CardWrapper>
+  );
 };
 
 export default LoginForm;
